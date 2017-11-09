8306e03c95bbf8aafc85171edfd218dd41f11f02
faaEstelle <estelle@cflab.net>
2017-11-09 10:08:45 +0800

    帧动画库设计：需求分析-接口编程-调用方式-代码设计

---
+++ FrameAnimation/rabbit.js
@@ -0,0 +1,32 @@
+var imgUrl = 'rabbit-big.png'
+var positions = ['0,-854', '-174,-852', '-349,-852', '-524,-852', '-698 -852', '-873 -848']
+var ele = document.getElementById('rabbit')
+
+animation(ele, positions, imgUrl)
+
+/**
+ *帧动画
+ * @param ele：要操作的DOM对象
+ * @param positions：图片位置数组
+ * @param imgUrl：图片资源路径
+ */
+function animation (ele, positions, imgUrl) {
+  ele.style.backgroundImage = 'url(' + imgUrl + ')'
+  ele.style.backgroundRepeat = 'no-repeat'
+  //当前索引（位置）
+  var index = 0
+
+  function run () {
+
+    var position = positions[index].split(' ')
+    console.log(position)
+    ele.style.backgroundPosition = position[0] + 'px ' + position[1] + 'px'
+    index++
+    if (index >= positions.length) {
+      index = 0
+    }
+    setTimeout(run, 80)
+  }
+
+  run()
+}
